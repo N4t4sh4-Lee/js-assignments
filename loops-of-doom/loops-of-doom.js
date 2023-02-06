d77f45106f57ffc3859360285aa4a2e5acb127ce
N4t4sh4-Lee <118701362+N4t4sh4-Lee@users.noreply.github.com>
2023-02-06 10:46:10 +0800

    loops of doom
    finished

---
+++ loops-of-doom/loops-of-doom.js
@@ -0,0 +1,61 @@
+for (i = 0; i < 5; i++) {
+    document.write(`I love web dev class! <br>`)
+}
+
+for (a = 0; a < 5; a++) {
+    document.write(`<br>`)
+    document.write(a)
+}
+
+document.write(`<br>`)
+
+for (c = 15; c < 30; c = c + 3) {
+    document.write(`<br>`)
+    document.write(c)
+}
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+for (d = 0; d < 4; d++) {
+    for (e = 0; e < 4; e++) {
+        document.write(`⬜`)
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for (f = 1; f < 9; f++) {
+    for (g = 1; g < f; g++) {
+        document.write(`🦴`)
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+for (aa = 1; aa <= 7; aa++) {
+    for (bb = 6; bb >= aa; bb--) {
+        document.write(`~`)
+    }
+    for (cc = 1; cc <= aa; cc++) {
+        document.write(`🦴`)
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+for (aaa = 1; aaa <= 5; aaa++) {
+    for (bbb = 1; bbb <= aaa; bbb++) {
+        document.write(`<br>`)
+        for (ccc = 1; ccc <= bbb; ccc++) {
+            document.write(`🦴`)
+        }
+    }
+    document.write(`<br>`)
+    document.write(`<br>`)
+}
